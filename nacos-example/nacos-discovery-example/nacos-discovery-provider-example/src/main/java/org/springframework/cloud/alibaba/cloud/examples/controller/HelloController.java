e5e0b1e07de086bf054f24fc929d9bdb0731908e
wzg923 <wzg923@126.com>
2019-07-10 19:07:40 +0800

    service-provider新增服务方法

--- nacos-example/nacos-discovery-example/nacos-discovery-provider-example/src/main/java/org/springframework/cloud/alibaba/cloud/examples/controller/HelloController.java
+++ nacos-example/nacos-discovery-example/nacos-discovery-provider-example/src/main/java/org/springframework/cloud/alibaba/cloud/examples/controller/HelloController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.TimeUnit;
+
 /**
  * @ClassName HelloController
  * @Description TODO
@@ -20,8 +22,19 @@ public class HelloController {
     @Value("${user.name}")
     String userName;
 
+
     @RequestMapping(value = "/hello/{string}", method = RequestMethod.GET)
-    public String echo2(@PathVariable String string) {
+    public String hello(@PathVariable String string) {
         return "Hello " + string + "!I am "+userName+".";
+    }
+
+    @RequestMapping(value = "/sleep/{time}")
+    public String sleep(@PathVariable Long  time){
+        try {
+            TimeUnit.SECONDS.sleep(time);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return "success";
     }
 }
